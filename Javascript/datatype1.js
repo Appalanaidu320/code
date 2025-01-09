c8f34e0d6052785acdb2f4091929ec1ce39f2dcf
Appalanaidu320 <appalanaiducm320@gmail.com>
2025-01-09 11:58:02 -0500

    Add files via upload

---
+++ Javascript/datatype1.js
@@ -0,0 +1,10 @@
+let name= 'appu';
+console.log(typeof name);
+let age=45;
+console.log(typeof age );
+ let isMarried = false;
+ console.log(typeof isMarried);
+
+
+ let selectColor= null;
+ console.log(typeof selectColor);
